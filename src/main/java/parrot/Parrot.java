6addba3f0e0223d011cdd5286aafdf822608da49
trikitrok <trikitrok@gmail.com>
2016-07-14 12:28:12 +0200

    removing obsolete code

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -2,37 +2,14 @@ package parrot;
 
 public class Parrot {
 
-    private ParrotTypeEnum type;
-    private int numberOfCoconuts = 0;
-    private double voltage;
-    private boolean isNailed;
-
-
-    private Parrot(ParrotTypeEnum _type, int numberOfCoconuts, double voltage, boolean isNailed) {
-        this.type = _type;
-        this.numberOfCoconuts = numberOfCoconuts;
-        this.voltage = voltage;
-        this.isNailed = isNailed;
-    }
-
-    protected Parrot() {
-
-    }
-
     public static Parrot create(ParrotTypeEnum _type, int numberOfCoconuts, double voltage, boolean isNailed) {
         if (_type.equals(ParrotTypeEnum.EUROPEAN)) {
             return new EuropeanParrot();
-        }
-
-        if (_type.equals(ParrotTypeEnum.AFRICAN)) {
+        } else  if (_type.equals(ParrotTypeEnum.AFRICAN)) {
             return new AfricanParrot(numberOfCoconuts);
-        }
-
-        if(_type.equals(ParrotTypeEnum.NORWEGIAN_BLUE)) {
+        } else {
             return new NorwegianBlueParrot(voltage, isNailed);
         }
-
-        return new Parrot(_type, numberOfCoconuts, voltage, isNailed);
     }
 
     public double getSpeed() {
